6d4484b6965a97785e5a6e34d7aa560d892a0cb1
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-10 15:47:21 -0500

    core-calculator-sandbox

--- Frontend/src/scenes/index.js
+++ Frontend/src/scenes/index.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import './styles.sass';
 
+import App from '../component/App';
+
 // import * as actions from '../redux/actions/actions';
 // const mapStateToProps = state => ({});
 // const mapDispatchToProps = dispatch => ({});
 
 const Scenes = ({}) => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div>
-      <div>Count {count}</div>
+      {/* <div>Count {count}</div>
 
       <button onClick={() => setCount(lastCount => lastCount + 1)}>
         Click Here to Increase the Count
-      </button>
+      </button> */}
+      <App />
     </div>
   );
 };
