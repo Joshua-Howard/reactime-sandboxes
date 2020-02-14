e4ddde0f928669d474f3911794ac04965f0adc49
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-14 11:55:47 -0500

    New astParser, linkFiber, tree, and Frontend index.js Comments

--- Frontend/index.js
+++ Frontend/index.js
@@ -8,3 +8,18 @@ const rootContainer = document.getElementById('root');
 ReactDOM.render(<App />, rootContainer);
 
 reactime(rootContainer);
+
+// const getCircularReplacer = () => {
+//   const seen = new WeakSet();
+//   return (key, value) => {
+//     if (typeof value === 'object' && value !== null) {
+//       if (seen.has(value)) {
+//         return;
+//       }
+//       seen.add(value);
+//     }
+//     return value;
+//   };
+// };
+
+// console.log(JSON.stringify(rootContainer, getCircularReplacer()));
