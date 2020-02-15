799530f4747dadec9f12a5b9d6f2fb4b1ca21a29
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-15 14:30:18 -0500

    Initial UseState Sandbox with 2 Tests

--- Frontend/src/scenes/sandboxes/useState.js
+++ Frontend/src/scenes/sandboxes/useState.js
@@ -6,19 +6,42 @@ import './styles.sass';
 // const mapStateToProps = state => ({});
 // const mapDispatchToProps = dispatch => ({});
 
-const Scenes = ({}) => {
+const UseState = ({}) => {
   const [count, setCount] = useState(0);
+
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(1000);
 
   return (
     <div>
-      <div>Count {count}</div>
+      <div>
+        <div>Count {count}</div>
 
-      <button onClick={() => setCount(lastCount => lastCount + 1)}>
-        Click Here to Increase the Count
-      </button>
+        <button onClick={() => setCount(lastCount => lastCount + 1)}>
+          Click Here to Increase the Count
+        </button>
+      </div>
+
+      <div className="bg-primary p-1 my-3 mx-5 vh-5 text-white text-center font-weight-bold">
+        New Test Below
+      </div>
+
+      <div>
+        <div>Count Increase {count2}</div>
+        <div>Count Decrease {count3}</div>
+
+        <button
+          onClick={() => {
+            setCount2(lastCount => lastCount + 1);
+            setCount3(lastCount => lastCount - 1);
+          }}
+        >
+          Click Here to Change 2 Counts Simultaneously
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Scenes;
+export default UseState;
 // export default connect(mapStateToProps, mapDispatchToProps)(Scenes);
