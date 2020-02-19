31a2a70c99af9a2374a33152924c30cdf85a664c
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-19 16:33:07 -0500

    Install react-router-dom and Add a Router Sandbox

--- Frontend/src/scenes/sandboxes/router.js
+++ Frontend/src/scenes/sandboxes/router.js
@@ -1,28 +1,57 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import UseState from './useState';
+import UseEffect from './useEffect';
+import UseContext from './useContext';
+import UseMemo from './useMemo';
+import AppContextProvider from '../../context/appContextProvider';
+
 import './styles.sass';
 
 // import * as actions from '../redux/actions/actions';
 // const mapStateToProps = state => ({});
 // const mapDispatchToProps = dispatch => ({});
 
-const Scenes = ({}) => {
-  const [count, setCount] = useState(0);
+const RouterApp = () => (
+  <Router>
+    <div className="pb-4">
+      <Link exact to="/" className="font-weight-bold mr-5">
+        Use State Route
+      </Link>
 
-  return (
-    <div>
-      To Be Completed
-      {/* <div>
-        Count
-        {` ${count}`}
-      </div>
+      <Link to="/useEffectRoute" className="font-weight-bold mr-5">
+        Use Effect Route
+      </Link>
 
-      <button onClick={() => setCount(lastCount => lastCount + 1)}>
-        Click Here to Increase the Count
-      </button> */}
+      <Link to="/useContextRoute" className="font-weight-bold mr-5">
+        Use Context Route
+      </Link>
+
+      <Link to="/useMemoRoute" className="font-weight-bold mr-5">
+        Use Memo Route
+      </Link>
     </div>
-  );
-};
+    <Switch>
+      <Route exact path="/">
+        <UseState />
+      </Route>
 
-export default Scenes;
+      <Route path="/useEffectRoute">
+        <UseEffect />
+      </Route>
+
+      <Route path="/useContextRoute">
+        <AppContextProvider>
+          <UseContext />
+        </AppContextProvider>
+      </Route>
+
+      <Route path="/useMemoRoute">
+        <UseMemo />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default RouterApp;
 // export default connect(mapStateToProps, mapDispatchToProps)(Scenes);
