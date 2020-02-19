391410dfbdda5130b2c84e695ec64dc21f72f344
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-19 17:49:05 -0500

    Initial Suspense Sandbox

--- Frontend/src/scenes/index.js
+++ Frontend/src/scenes/index.js
@@ -1,127 +1,59 @@
-import React, { useState } from 'react';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
-// import { connect } from 'react-redux';
-import './styles.sass';
+import React, { Suspense, useState } from 'react';
+import { fetchProfileData } from './fakeApi';
+
 import UseState from './sandboxes/useState';
-import UseEffect from './sandboxes/useEffect';
-import UseContext from './sandboxes/useContext';
-import UseMemo from './sandboxes/useMemo';
-import Redux from './sandboxes/redux';
-import Router from './sandboxes/router';
-import SetState from './sandboxes/setState';
-import ComponentDidMount from './sandboxes/componentDidMount';
-import AppContextProvider from '../context/appContextProvider';
+import Sandboxes from './sandboxes';
 
-// import * as actions from '../redux/actions/actions';
-// const mapStateToProps = state => ({});
-// const mapDispatchToProps = dispatch => ({});
+const initialResource = fetchProfileData();
 
-const Scenes = () => {
-  const [activeSandbox, setActiveSandbox] = useState('UseState');
+function ProfilePage() {
+  const [resource, setResource] = useState(initialResource);
 
-  function changeSandbox(e) {
-    const { innerText } = e.target;
-    setActiveSandbox(innerText);
-  }
-
-  function sandboxButtons() {
-    const buttonsArray = [
-      'UseState',
-      'UseEffect',
-      'UseContext',
-      'UseMemo',
-      '|',
-      'Redux',
-      'Router',
-      '|',
-      'SetState',
-      'ComponentDidMount'
-    ];
-
-    const buttons = buttonsArray.map((buttonName, index) => {
-      if (buttonName === '|') {
-        return <div key={buttonName + index}>{buttonName}</div>;
-      }
-
-      return (
-        <button
-          key={buttonName + index}
-          type="button"
-          onClick={changeSandbox}
-          style={
-            activeSandbox === buttonName
-              ? { outline: 'none', backgroundColor: 'cornflowerblue' }
-              : null
-          }
-        >
-          {buttonName}
-        </button>
-      );
-    });
-
-    return <>{buttons}</>;
-  }
-
-  function displaySandbox() {
-    switch (activeSandbox) {
-      case 'UseState':
-        return <UseState />;
-
-      case 'UseEffect':
-        return <UseEffect />;
-
-      case 'UseContext':
-        return (
-          <AppContextProvider>
-            <UseContext />
-          </AppContextProvider>
-        );
-
-      case 'UseMemo':
-        return <UseMemo />;
-
-      case 'Redux':
-        return (
-          <Provider store={store}>
-            <Redux />
-          </Provider>
-        );
-
-      case 'Router':
-        return <Router />;
-
-      case 'SetState':
-        return <SetState />;
-
-      case 'ComponentDidMount':
-        return <ComponentDidMount />;
-
-      default:
-        return <UseState />;
-    }
+  function handleRefreshClick() {
+    setResource(fetchProfileData());
   }
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col d-flex justify-content-around m-3">
-          {sandboxButtons()}
-        </div>
+    <>
+      <Suspense fallback={<h1>Loading profile...</h1>}>
+        <ProfileDetails resource={resource} />
+        <button type="button" onClick={handleRefreshClick}>
+          Refresh
+        </button>
+        <Suspense fallback={<h1>Loading posts...</h1>}>
+          <ProfileTimeline resource={resource} />
+          <div className="font-weight-bold">
+            The useState child below is a child of Suspense
+          </div>
+          <UseState />
+        </Suspense>
+      </Suspense>
+      <div className="font-weight-bold">
+        The useState child below is a sibling of Suspense
       </div>
-
-      <div className="row">
-        <div className="col text-white bg-secondary text-center font-weight-bold p-1">
-          Sandbox Below
-        </div>
+      <UseState />
+      {/* <div className="font-weight-bold text-center mt-5 mb-2">
+        All sandboxes are below - they are a sibling of suspense
       </div>
-
-      <div className="row">
-        <div className="col m-5 justify-content-center">{displaySandbox()}</div>
-      </div>
-    </div>
+      <Sandboxes /> */}
+    </>
   );
-};
+}
 
-export default Scenes;
-// export default connect(mapStateToProps, mapDispatchToProps)(Scenes);
+function ProfileDetails({ resource }) {
+  const user = resource.user.read();
+  return <h1>{user.name}</h1>;
+}
+
+function ProfileTimeline({ resource }) {
+  const posts = resource.posts.read();
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ProfilePage;
