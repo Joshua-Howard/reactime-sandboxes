26f3f6cf45b9986e32a8b170a23d37e8b6c35d14
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-19 16:08:49 -0500

    Add a Child Component to useState

--- Frontend/src/scenes/sandboxes/useStateChild.js
+++ Frontend/src/scenes/sandboxes/useStateChild.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import UseStateChild from './useStateChild';
 // import { connect } from 'react-redux';
 import './styles.sass';
 
@@ -7,33 +6,35 @@ import './styles.sass';
 // const mapStateToProps = state => ({});
 // const mapDispatchToProps = dispatch => ({});
 
-const UseState = () => {
-  const [count, setCount] = useState(0);
+const UseStateChild = () => {
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(1000);
 
   return (
     <div>
       <div>
         <div>
-          Count
-          {` ${count}`}
+          Count Increase
+          {` ${count2}`}
+        </div>
+        <div>
+          Count Decrease
+          {` ${count3}`}
         </div>
 
         <button
           type="button"
-          onClick={() => setCount(lastCount => lastCount + 1)}
+          onClick={() => {
+            setCount2(lastCount => lastCount + 1);
+            setCount3(lastCount => lastCount - 1);
+          }}
         >
-          Click Here to Increase the Count
+          Click Here to Change both Counts Simultaneously
         </button>
       </div>
-
-      <div className="bg-primary p-1 my-3 mx-5 vh-5 text-white text-center font-weight-bold">
-        New Test Below
-      </div>
-
-      <UseStateChild />
     </div>
   );
 };
 
-export default UseState;
+export default UseStateChild;
 // export default connect(mapStateToProps, mapDispatchToProps)(Scenes);
