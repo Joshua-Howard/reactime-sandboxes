4d0edf1b84b8dd0f59aee0340a06badee6b831af
Josh <Joshua-Howard@users.noreply.github.com>
2020-02-20 15:55:33 -0500

    npm install typescript, create tsconfig, and update the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'Frontend', 'index.js'),
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  entry: path.join(__dirname, 'Frontend', 'index.tsx'),
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
       },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      },
+      // {
+      //   test: /\.jsx?/,
+      //   exclude: /node_modules/,
+      //   use: ['babel-loader']
+      // },
       {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader']
